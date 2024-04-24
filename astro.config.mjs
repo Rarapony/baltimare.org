23507c04a49e2a742b1f5ced35c6e8a5c5d7595d
15 <15@soiree.horse>
2024-04-24 04:15:39 -0700

    SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import icon from "astro-icon";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     prefetchAll: true
   },
   integrations: [tailwind(), vue(), icon()],
-  output: "server"
+  output: "server",
+  adapter: cloudflare()
 });
