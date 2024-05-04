18d7072506f29c32548ea786ec401734eef62d44
15 <15@soiree.horse>
2024-05-03 21:37:23 -0700

    Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import icon from "astro-icon";
 
 // import cloudflare from "@astrojs/cloudflare";
 
+import playformCompress from "@playform/compress";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://baltimare.org",
   prefetch: {
     prefetchAll: true
   },
-  integrations: [tailwind(), vue(), icon()],
+  integrations: [tailwind(), vue(), icon(), playformCompress(), sitemap()]
   // output: "hybrid",
   // adapter: cloudflare()
 });
